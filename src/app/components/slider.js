a6959702c5555cdf511fcc5f6cea4227a5935866
Chris Kavanagh <ckava3@gmail.com>
2023-12-10 20:19:03 -0500

    slider component

--- src/app/components/slider.js
+++ src/app/components/slider.js
@@ -1,3 +1,31 @@
+"use client";
+
 import React from "react";
+//import * as pics from "/img";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
+import Image from "next/image";
+
+export default function Slider() {
+  const [sliderRef] = useKeenSlider({
+    mode: "free-snap",
+    slides: {
+      origin: "center",
+      perView: 2,
+      spacing: 15,
+    },
+  });
+
+  return (
+    <div ref={sliderRef} className="keen-slider">
+      <div className="number-slide1">
+        <Image src="/img/pic1.jpg" width={600} height={500} alt="img1" />
+      </div>
+      <div className="keen-slider__slide number-slide2">2</div>
+      <div className="keen-slider__slide number-slide3">3</div>
+      <div className="keen-slider__slide number-slide4">4</div>
+      <div className="keen-slider__slide number-slide5">5</div>
+      <div className="keen-slider__slide number-slide6">6</div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Slider from "./components/slider";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>test</h1>
+      <Slider />
     </main>
   );
 }
