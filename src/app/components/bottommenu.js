ce1bb8c51ce097670aeaae7ca5d0328c864271d1
Chris Kavanagh <ckava3@gmail.com>
2023-12-27 23:54:31 -0500

    css

--- src/app/components/bottommenu.js
+++ src/app/components/bottommenu.js
@@ -1,13 +1,15 @@
+import styles from "../styles/bottommenu.module.css";
+
 export default function BottomMenu({ children }) {
   return (
-    <section className="btmmenu__wrapper">
-      <div className="btmmenu">
-        <ul className="menu__list">
-          <li className="menu__item">Roofs</li>
-          <li className="menu__item">Additions</li>
-          <li className="menu__item">Remodeling</li>
-          <li className="menu__item">Kitchen & Baths</li>
-          <li className="menu__item">Decks & Porches</li>
+    <section className={styles.btmmenu__wrapper}>
+      <div className={styles.btmmenu}>
+        <ul className={styles.menu__list}>
+          <li className={styles.menu__item}>Roofs</li>
+          <li className={styles.menu__item}>Additions</li>
+          <li className={styles.menu__item}>Remodeling</li>
+          <li className={styles.menu__item}>Kitchen & Baths</li>
+          <li className={styles.menu__item}>Decks & Porches</li>
         </ul>
       </div>
     </section>
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,39 +1,39 @@
 "use client";
 
 import React, { useState } from "react";
-import "../globals.css";
-import styles from "../styles"
+//import "../globals.css";
+import styles from "../styles/navbar.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { AiFillHome, AiFillFormatPainter, AiFillPhone, AiOutlineTeam } from "react-icons/ai";
 
 export default function Navbar({ children }) {
   const pathname = usePathname();
   return (
     <section className={styles.nav__wrapper}>
-    <nav className={styles.primary__nav}>
-        <div className="logo">
-          <span>Nester</span> Construction LLC
+      <nav className={styles.primary__nav}>
+        <div className={styles.logo}>
+          <span className={styles.logo__name}>Nester</span> Construction LLC
         </div>
-        <ul className="navbar__list">
-          <li className="nav__item">
+        <ul className={styles.navbar__list}>
+          <li className={`${styles.nav__item} ${styles.nav__hover}`}>
             <Link className={`link ${pathname === "/" ? "active" : ""}`} href="/">
-              Home
+              <AiFillHome /> Home
             </Link>
           </li>
-          <li>
+          <li className={`${styles.nav__item} ${styles.nav__hover}`}>
             <Link className={`link ${pathname === "/services" ? "active" : ""}`} href="/services">
-              Services
+              <AiFillFormatPainter /> Services
             </Link>
           </li>
-          <li>
+          <li className={`${styles.nav__item} ${styles.nav__hover}`}>
             <Link className={`link ${pathname === "/about" ? "active" : ""}`} href="/about">
-              About Us
+              <AiOutlineTeam /> About Us
             </Link>
           </li>
-          <li>
+          <li className={`${styles.nav__item} ${styles.nav__hover}`}>
             <Link className={`link ${pathname === "/contact" ? "active" : ""}`} href="/contact">
-              Contact
+              <AiFillPhone /> Contact
             </Link>
           </li>
         </ul>
