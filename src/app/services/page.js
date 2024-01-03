372a36a9019ee956ee4d03346bb96f234e84d196
Chris Kavanagh <ckava3@gmail.com>
2024-01-03 15:54:55 -0500

    services section css

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,3 +1,11 @@
+import styles from "./page.module.css";
+
 export default function Services() {
-  return <section className={styles.services}></section>;
+  return (
+    <section className={styles.services}>
+      <div>
+        <h1>Our Services</h1>
+      </div>
+    </section>
+  );
 }
