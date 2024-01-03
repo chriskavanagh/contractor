d0a3e93a60c0eb9e123c7025ba147c88ae7d1c3a
Chris Kavanagh <ckava3@gmail.com>
2024-01-03 16:26:48 -0500

    services css

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -4,7 +4,15 @@ export default function Services() {
   return (
     <section className={styles.services}>
       <div>
-        <h1>Our Services</h1>
+        <h1 className={styles.title}>Our Services</h1>
+      </div>
+      <div className={styles.content__wrapper}>
+        <div className={styles.item}>Roofing</div>
+        <div className={styles.item}>Remodelling</div>
+        <div className={styles.item}>Additions</div>
+        <div className={styles.item}>Decks</div>
+        <div className={styles.item}>Porches</div>
+        <div className={styles.item}>Kitchens</div>
       </div>
     </section>
   );
