24fa71dba378d67914b637cefe60ea2cef7633f1
Chris Kavanagh <ckava3@gmail.com>
2024-01-04 08:13:03 -0500

    bottommenu css

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,3 +1,22 @@
+import styles from "./page.module.css";
+
 export default function Services() {
-  return <section className={styles.services}></section>;
+  return (
+    <section className={styles.services}>
+      <div>
+        <h1 className={styles.title}>Our Services</h1>
+      </div>
+      <div>
+        <h3 className={`${styles.subtitle} ${styles.center__text}`}>Make Room For Your Memories</h3>
+      </div>
+      <div className={styles.content__wrapper}>
+        <div className={styles.item}>Roofing</div>
+        <div className={styles.item}>Remodelling</div>
+        <div className={styles.item}>Additions</div>
+        <div className={styles.item}>Decks</div>
+        <div className={styles.item}>Porches</div>
+        <div className={styles.item}>Kitchens</div>
+      </div>
+    </section>
+  );
 }
