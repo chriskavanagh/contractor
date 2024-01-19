5d2cfb96158ce5012b1357603c6dd1b4910969e0
Chris Kavanagh <ckava3@gmail.com>
2024-01-19 18:07:24 -0500

    overlay on Home.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,32 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Slider from "./components/slider";
 import MainContent from "./components/maincontent";
 import styles from "./page.module.css";
 //import React, { useState, useEffect } from "react";
 //import { useRouter } from "next/navigation";
-//import Overlay from "./components/overlay";
+import Overlay from "./components/overlay";
 
 export default function Home() {
-  //console.log(setClientWindowHeight);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <main className={styles.main}>
+      {/* <h2>scroll position: {scrollPosition}</h2> */}
+      {scrollPosition === 0 && <Overlay />}
+      {/* {scrollPosition > 0 && <h2>Robbie Sucks Dick</h2>} */}
       <Slider />
       <MainContent />
     </main>
