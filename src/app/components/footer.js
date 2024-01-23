8e985cbd10eadcfb68cb28fbb157b9e6c4bc6582
Chris Kavanagh <ckava3@gmail.com>
2024-01-22 22:42:03 -0500

    css

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,4 +1,5 @@
 import styles from "../styles/footer.module.css";
+import "../globals.css";
 
 export default function Footer() {
   return (
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,12 @@
+import styles from "./page.module.css";
+import "../globals.css";
+
 export default function Contact() {
   return (
-    <section className="contact">
-      <div className="wrapper">
-        <h1>How to contact us.</h1>
-        <h3>Phone: 540-397-8715</h3>
+    <section className={styles.contact}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.header}>How to contact us</h1>
+        <h3 className={styles.phone}>Phone: 540-397-8715</h3>
       </div>
     </section>
   );
