8f8cd9e5019cce11188c8cc70a7009b33bc14dc6
Chris Kavanagh <ckava3@gmail.com>
2024-01-23 16:46:38 -0500

    grid

--- src/app/components/bottommenu.js
+++ src/app/components/bottommenu.js
@@ -27,19 +27,19 @@ export default function BottomMenu({ children }) {
             <span className={styles.icon__space}>
               <PiWarehouseFill color="#FFD700" />
             </span>
-            Remodeling
+            Remodel
           </li>
           <li className={styles.menu__item}>
             <span className={styles.icon__space}>
               <GiKitchenTap color="#FFD700" />
             </span>
-            Kitchen & Baths
+            Kitchen/Baths
           </li>
           <li className={styles.menu__item}>
             <span className={styles.icon__space}>
               <MdOutlineDeck color="#FFD700" />
             </span>
-            Decks & Porches
+            Decks/Porches
           </li>
         </ul>
       </div>
