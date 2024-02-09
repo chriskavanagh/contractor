823283db6dc609cc6ea6434ee943942dd5300964
Chris Kavanagh <ckava3@gmail.com>
2024-02-09 15:19:01 -0500

    close button

--- src/app/components/sidedrawer.js
+++ src/app/components/sidedrawer.js
@@ -1,21 +1,27 @@
 "use client";
 
 import "../globals.css";
+import Link from "next/link";
 import { useContext } from "react";
 import { Drawer } from "../context/context";
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "../styles/sidedrawer.module.css";
+import { IoCloseCircleOutline } from "react-icons/io5";
 import { AiFillHome, AiFillFormatPainter, AiFillPhone, AiOutlineTeam } from "react-icons/ai";
 
 export default function Sidedrawer() {
-  const { drawer } = useContext(Drawer);
-  console.log(drawer);
   const pathname = usePathname();
+  const { drawer, setDrawer } = useContext(Drawer);
+
+  const clickHandler = e => {
+    console.log(drawer);
+    setDrawer(false);
+  };
   return (
     <section className={styles.drawer__wrapper}>
       {drawer == true && (
         <nav className={`${styles.nav} ${drawer === true ? " open" : ""}`}>
+          <IoCloseCircleOutline className={styles.close} onClick={clickHandler} />
           <ul className={styles.list}>
             <li className={styles.list__item}>
               <Link className={`link ${pathname === "/" ? "active" : ""}`} href="/">
