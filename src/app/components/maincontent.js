9753a0b94aa209b5e915367487e096a453fac1cd
Chris Kavanagh <ckava3@gmail.com>
2024-02-15 23:05:14 -0500

    css

--- src/app/components/maincontent.js
+++ src/app/components/maincontent.js
@@ -6,20 +6,22 @@ export default function MainContent() {
   return (
     <section className={styles.mainContent}>
       <div>
-        <h1 className={styles.title}>Nester Contruction &amp; Remodeling</h1>
+        <h1 className={styles.title}>Nester Construction &amp; Remodeling</h1>
         <h2 className={styles.subtitle}>
           Your Go To Source For All Construction/Remodeling Needs & More!
         </h2>
       </div>
       <div className={styles.decription}>
         <p className={styles.para}>
-          Nester Construction is a family owned business which has served the Roanoke Valley, Salem,
-          Vinton, Bedford area for over 30 years. We also work in North Carolina.
+          Nester Construction is a family owned business which has served the
+          Roanoke Valley, Salem, Vinton, Bedford area for over 30 years. We also
+          work in North Carolina.
         </p>
         <br />
         <p className={styles.para}>
-          Old, leaky roof? Your home needs a coat of fresh paint? Nester Construction/Remodelling
-          will install your new roof and/or paint your house so it looks great indoors and outdoors.
+          Old, leaky roof? Your home needs a coat of fresh paint? Nester
+          Construction/Remodelling will install your new roof and/or paint your
+          house so it looks great indoors and outdoors.
         </p>
         <br />
         <p className={styles.para}>
@@ -27,12 +29,15 @@ export default function MainContent() {
         </p>
         <br />
         <p className={styles.para}>
-          Don't wait any longer for your new kitchen or sunroom. Now is the time for a new addition
-          to share with family and friends and create memories that will last a lifetime.
+          Don't wait any longer for your new kitchen or sunroom. Now is the time
+          for a new addition to share with family and friends and create
+          memories that will last a lifetime.
         </p>
       </div>
       <div className={styles.construction__experts}>
-        <h2 className={styles.experts}>Call the Construction/Remodelling Experts Today</h2>
+        <h2 className={styles.experts}>
+          Call the Construction/Remodelling Experts Today
+        </h2>
         <br />
         <span className={styles.phone}>
           <AiFillPhone color="rgb(142, 5, 5)" /> 540-397-8715
