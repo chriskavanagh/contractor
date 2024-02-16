df244324b191481f541d4dd37b859035ddcde955
Chris Kavanagh <ckava3@gmail.com>
2024-02-16 03:22:31 -0500

    font size icon

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -8,7 +8,12 @@ import styles from "../styles/navbar.module.css";
 import { usePathname } from "next/navigation";
 //import DrawerBtn from "./drawerbtn";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { AiFillHome, AiFillFormatPainter, AiFillPhone, AiOutlineTeam } from "react-icons/ai";
+import {
+  AiFillHome,
+  AiFillFormatPainter,
+  AiFillPhone,
+  AiOutlineTeam,
+} from "react-icons/ai";
 
 // ---Top Navbar---
 export default function Navbar({ children }) {
@@ -16,14 +21,18 @@ export default function Navbar({ children }) {
   //const [sideDrawer, setsideDrawer] = useState(false);
   const pathname = usePathname();
 
-  const clickHandler = e => {
+  const clickHandler = (e) => {
     console.log(drawer);
     setDrawer(!drawer);
   };
 
   return (
     <section className={styles.nav__wrapper}>
-      <GiHamburgerMenu className={styles.burger} onClick={clickHandler} />
+      <GiHamburgerMenu
+        size={"1.2em"}
+        className={styles.burger}
+        onClick={clickHandler}
+      />
       <nav className={styles.primary__nav}>
         <div className={styles.logo}>
           <span className={styles.logo__name}>Nester</span> Construction LLC
@@ -31,22 +40,34 @@ export default function Navbar({ children }) {
 
         <ul className={styles.navbar__list}>
           <li className={`${styles.nav__item} ${styles.nav__hover}`}>
-            <Link className={`link ${pathname === "/" ? "active" : ""}`} href="/">
+            <Link
+              className={`link ${pathname === "/" ? "active" : ""}`}
+              href="/"
+            >
               <AiFillHome className={styles.icon__react} /> Home
             </Link>
           </li>
           <li className={`${styles.nav__item} ${styles.nav__hover}`}>
-            <Link className={`link ${pathname === "/services" ? "active" : ""}`} href="/services">
+            <Link
+              className={`link ${pathname === "/services" ? "active" : ""}`}
+              href="/services"
+            >
               <AiFillFormatPainter className={styles.icon__react} /> Services
             </Link>
           </li>
           <li className={`${styles.nav__item} ${styles.nav__hover}`}>
-            <Link className={`link ${pathname === "/about" ? "active" : ""}`} href="/about">
+            <Link
+              className={`link ${pathname === "/about" ? "active" : ""}`}
+              href="/about"
+            >
               <AiOutlineTeam className={styles.icon__react} /> About
             </Link>
           </li>
           <li className={`${styles.nav__item} ${styles.nav__hover}`}>
-            <Link className={`link ${pathname === "/contact" ? "active" : ""}`} href="/contact">
+            <Link
+              className={`link ${pathname === "/contact" ? "active" : ""}`}
+              href="/contact"
+            >
               <AiFillPhone className={styles.icon__react} /> Contact
             </Link>
           </li>
