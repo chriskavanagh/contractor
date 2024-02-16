ce02c345eb20dda91769b3192dc75262b0b16814
Chris Kavanagh <ckava3@gmail.com>
2024-02-16 03:33:34 -0500

    css

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -29,7 +29,7 @@ export default function Navbar({ children }) {
   return (
     <section className={styles.nav__wrapper}>
       <GiHamburgerMenu
-        size={"1.9em"}
+        size={"2.3em"}
         className={styles.burger}
         onClick={clickHandler}
       />
