c7ad70ce3a2574914c6ebcb4261b7ec754ce8666
Chris Kavanagh <ckava3@gmail.com>
2024-02-16 04:20:38 -0500

    css

--- src/app/components/sidedrawer.js
+++ src/app/components/sidedrawer.js
@@ -28,7 +28,7 @@ export default function Sidedrawer() {
         <>
           <nav className={`${styles.nav} ${drawer === true ? " open" : ""}`}>
             <GiHamburgerMenu
-              size={"2.7em"}
+              size={"2.9em"}
               className={styles.close}
               onClick={clickHandler}
             />
